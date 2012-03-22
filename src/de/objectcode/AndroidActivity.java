88a66fb62183a60b7c0641b9ff3bafc37e65a54c
Philipp Schumacher <PhilippSchum@web.de>
2012-03-22 16:12:44 +0100

    package fix

--- src/de/objectcode/AndroidActivity.java
+++ src/de/objectcode/AndroidActivity.java
@@ -7,31 +7,35 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+
 public class AndroidActivity extends ListActivity {
-    String tests[] = { "LifeCycleTest", "SingleTouchTest", "MultiTouchTest",
-            "KeyTest", "AccelerometerTest", "AssetsTest",
-            "ExternalStorageTest", "SoundPoolTest", "MediaPlayerTest",
-            "FullScreenTest", "WakeLockTest", "RenderViewTest", "ShapeTest", "BitmapTest",
-            "FontTest", "SurfaceViewTest" };
+  String[] tests = {
+    "LifeCycleTest", "SingleTouchTest", "MultiTouchTest",
+    "KeyTest", "AccelerometerTest", "AssetsTest",
+    "ExternalStorageTest", "SoundPoolTest", "MediaPlayerTest",
+    "FullScreenTest", "WakeLockTest", "RenderViewTest", "ShapeTest", "BitmapTest",
+    "FontTest", "SurfaceViewTest"
+  };
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, tests));
-    }
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setListAdapter(new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, tests));
+  }
 
-    @SuppressWarnings("rawtypes")
-	@Override
-    protected void onListItemClick(ListView list, View view, int position,
-            long id) {
-        super.onListItemClick(list, view, position, id);
-        String testName = tests[position];
-        try {
-            Class clazz = Class.forName("com.badlogic.androidgames." + testName);
-            Intent intent = new Intent(this, clazz);
-            startActivity(intent);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+  @Override
+  @SuppressWarnings("rawtypes")
+  protected void onListItemClick(ListView list, View view, int position,
+    long id) {
+    super.onListItemClick(list, view, position, id);
+
+    String testName = tests[position];
+    try {
+      Class clazz = Class.forName("de.objectcode." + testName);
+      Intent intent = new Intent(this, clazz);
+      startActivity(intent);
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
     }
+  }
 }
